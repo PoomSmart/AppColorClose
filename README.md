5dd0295c1d112de6990d92869c26fa0baff722e1
PoomSmart <poomst@hotmail.com>
2015-06-16 17:29:03 +0700

    +

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ AppColorClose
 
 App's close box based on app icon.
 
-Requires iOS 7
+Requires iOS 7+
 
 ![alt tag](https://raw.github.com/PoomSmart/AppColorClose/master/SS.PNG)
+
+MIT License.
--- Tweak.mm
+++ Tweak.mm
@@ -1,11 +1,12 @@
-#import <UIKit/UIKit.h>
+#import "../PS.h"
 
 @interface SBIcon : NSObject
-- (UIImage *)getIconImage:(int)type;
+- (UIImage *)getIconImage:(NSInteger)type;
 @end
 
 @interface SBIconView : UIView
 @property(retain, nonatomic) SBIcon *icon;
+- (BOOL)_isShowingCloseBox;
 @end
 
 @interface SBCloseBoxView : UIView
@@ -14,33 +15,25 @@
 @interface SBIconBlurryBackgroundView : UIView
 @end
 
-@interface UIImage (Dominant)
-- (UIColor *)dominantColor;
-@end
-
-@interface UIImage (Addition)
-- (UIImage *)_flatImageWithColor:(UIColor *)color;
-@end
-
 struct pixel {
     unsigned char r, g, b, a;
 };
 
-@implementation UIImage (Dominant)
- 
-- (UIColor *)dominantColor
+static UIColor *dominantColorFromIcon(SBIcon *icon)
 {
+	UIImage *iconImage = [icon getIconImage:2];
 	NSUInteger red = 0;
 	NSUInteger green = 0;
 	NSUInteger blue = 0;
-	struct pixel *pixels = (struct pixel *)calloc(1, self.size.width * self.size.height * sizeof(struct pixel));
+	CGImageRef iconCGImage = iconImage.CGImage;
+	struct pixel *pixels = (struct pixel *)calloc(1, iconImage.size.width * iconImage.size.height * sizeof(struct pixel));
 	if (pixels != nil)
     {
-		CGContextRef context = CGBitmapContextCreate((void*)pixels, self.size.width, self.size.height, 8, self.size.width * 4, CGImageGetColorSpace(self.CGImage), kCGImageAlphaPremultipliedLast);
+		CGContextRef context = CGBitmapContextCreate((void *)pixels, iconImage.size.width, iconImage.size.height, 8, iconImage.size.width * 4, CGImageGetColorSpace(iconCGImage), kCGImageAlphaPremultipliedLast);
 		if (context != NULL) {
-			CGContextDrawImage(context, CGRectMake(0.0f, 0.0f, self.size.width, self.size.height), self.CGImage);
-			NSUInteger numberOfPixels = self.size.width * self.size.height;
-			for (int i=0; i<numberOfPixels; i++) {
+			CGContextDrawImage(context, CGRectMake(0.0f, 0.0f, iconImage.size.width, iconImage.size.height), iconCGImage);
+			NSUInteger numberOfPixels = iconImage.size.width * iconImage.size.height;
+			for (int i = 0; i < numberOfPixels; i++) {
 				red += pixels[i].r;
 				green += pixels[i].g;
 				blue += pixels[i].b;
@@ -55,27 +48,28 @@ struct pixel {
 	return [UIColor colorWithRed:red/255.0f green:green/255.0f blue:blue/255.0f alpha:1.0f];
 }
 
-@end
-
 %hook SBIconView
 
 - (void)_updateCloseBoxAnimated:(BOOL)animated
 {
 	%orig;
 	SBCloseBoxView *closeBox = MSHookIvar<SBCloseBoxView *>(self, "_closeBox");
-	if (closeBox != nil) {
-		UIImage *iconImage = [self.icon getIconImage:2];
-		UIColor *dominantColor = [iconImage dominantColor];
+	if (closeBox != nil && closeBox.hidden == NO) {
+		UIColor *dominantColor = dominantColorFromIcon(self.icon);
 		[MSHookIvar<UIView *>(closeBox, "_whiteTintView") setBackgroundColor:dominantColor];
 		id r = [[NSUserDefaults standardUserDefaults] objectForKey:@"SBCloseBoxTintAlpha"];
+		#if CGFLOAT_IS_DOUBLE
+		CGFloat tintAlpha = r ? [r doubleValue] : 0.65f;
+		#else
 		CGFloat tintAlpha = r ? [r floatValue] : 0.65f;
+		#endif
 		MSHookIvar<UIView *>(closeBox, "_whiteTintView").alpha = tintAlpha;
 
-		MSHookIvar<UIView *>(closeBox, "_whiteTintView").layer.borderWidth = 2;
+		MSHookIvar<UIView *>(closeBox, "_whiteTintView").layer.borderWidth = 2.0f;
 		MSHookIvar<UIView *>(closeBox, "_whiteTintView").layer.borderColor = dominantColor.CGColor;
 
-		[MSHookIvar<UIImageView *>(closeBox, "_xColorBurnView") setImage:[MSHookIvar<UIImageView *>(closeBox, "_xColorBurnView").image _flatImageWithColor:dominantColor]];
-		[MSHookIvar<UIView *>(closeBox, "_xColorBurnView") setAlpha:tintAlpha];
+		MSHookIvar<UIImageView *>(closeBox, "_xColorBurnView").image = [MSHookIvar<UIImageView *>(closeBox, "_xColorBurnView").image _flatImageWithColor:dominantColor];
+		MSHookIvar<UIView *>(closeBox, "_xColorBurnView").alpha = tintAlpha;
 	}
 }
 
