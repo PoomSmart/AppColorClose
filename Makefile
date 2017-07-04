b4e7d3366be5bdc0383cb6b228dd475c844217a4
Thatchapon Unprasert <poomst@hotmail.com>
2017-07-04 10:24:03 +0700

    Make it compile latest.

--- Makefile
+++ Makefile
@@ -1,11 +1,15 @@
-GO_EASY_ON_ME = 1
-ARCHS = armv7 arm64
-SDKVERSION = 7.0
+DEBUG = 0
+PACKAGE_VERSION = 1.1
 
-include theos/makefiles/common.mk
+ifeq ($(SIMULATOR),1)
+	TARGET = simulator:clang:latest
+	ARCHS = x86_64 i386
+endif
+
+include $(THEOS)/makefiles/common.mk
 TWEAK_NAME = AppColorClose
 AppColorClose_FILES = Tweak.xm
-AppColorClose_FRAMEWORKS = CoreGraphics QuartzCore UIKit
+AppColorClose_USE_SUBSTRATE = 1
 
 include $(THEOS_MAKE_PATH)/tweak.mk
 
@@ -13,3 +17,10 @@ internal-stage::
 	$(ECHO_NOTHING)mkdir -p $(THEOS_STAGING_DIR)/Library/PreferenceLoader/Preferences$(ECHO_END)
 	$(ECHO_NOTHING)cp -R Resources $(THEOS_STAGING_DIR)/Library/PreferenceLoader/Preferences/AppColorClose$(ECHO_END)
 	$(ECHO_NOTHING)find $(THEOS_STAGING_DIR) -name .DS_Store | xargs rm -rf$(ECHO_END)
+
+all::
+ifeq ($(SIMULATOR),1)
+	@rm -f /opt/simject/$(TWEAK_NAME).dylib
+	@cp -v $(THEOS_OBJ_DIR)/$(TWEAK_NAME).dylib /opt/simject
+	@cp -v $(PWD)/$(TWEAK_NAME).plist /opt/simject
+endif
--- Tweak.mm
+++ Tweak.mm
@@ -1,4 +1,5 @@
 #import "../PS.h"
+#import <UIKit/UIImage+Private.h>
 
 @interface SBIcon : NSObject
 - (UIImage *)getIconImage:(NSInteger)type;
@@ -19,58 +20,60 @@ struct pixel {
     unsigned char r, g, b, a;
 };
 
-static UIColor *dominantColorFromIcon(SBIcon *icon)
-{
-	UIImage *iconImage = [icon getIconImage:2];
-	NSUInteger red = 0;
-	NSUInteger green = 0;
-	NSUInteger blue = 0;
-	CGImageRef iconCGImage = iconImage.CGImage;
-	struct pixel *pixels = (struct pixel *)calloc(1, iconImage.size.width * iconImage.size.height * sizeof(struct pixel));
-	if (pixels != nil)
-    {
-		CGContextRef context = CGBitmapContextCreate((void *)pixels, iconImage.size.width, iconImage.size.height, 8, iconImage.size.width * 4, CGImageGetColorSpace(iconCGImage), kCGImageAlphaPremultipliedLast);
-		if (context != NULL) {
-			CGContextDrawImage(context, CGRectMake(0.0f, 0.0f, iconImage.size.width, iconImage.size.height), iconCGImage);
-			NSUInteger numberOfPixels = iconImage.size.width * iconImage.size.height;
-			for (int i = 0; i < numberOfPixels; i++) {
-				red += pixels[i].r;
-				green += pixels[i].g;
-				blue += pixels[i].b;
-			}
-			red /= numberOfPixels;
-			green /= numberOfPixels;
-			blue /= numberOfPixels;
-			CGContextRelease(context);
-		}
-		free(pixels);
-	}
-	return [UIColor colorWithRed:red/255.0f green:green/255.0f blue:blue/255.0f alpha:1.0f];
+static UIColor *dominantColorFromIcon(SBIcon *icon) {
+    UIImage *iconImage = [icon getIconImage:2];
+    NSUInteger red = 0;
+    NSUInteger green = 0;
+    NSUInteger blue = 0;
+    CGImageRef iconCGImage = iconImage.CGImage;
+    struct pixel *pixels = (struct pixel *)calloc(1, iconImage.size.width * iconImage.size.height * sizeof(struct pixel));
+    if (pixels) {
+        CGContextRef context = CGBitmapContextCreate((void *)pixels, iconImage.size.width, iconImage.size.height, 8, iconImage.size.width * 4, CGImageGetColorSpace(iconCGImage), kCGImageAlphaPremultipliedLast);
+        if (context != NULL) {
+            CGContextDrawImage(context, CGRectMake(0.0, 0.0, iconImage.size.width, iconImage.size.height), iconCGImage);
+            NSUInteger numberOfPixels = iconImage.size.width * iconImage.size.height;
+            for (int i = 0; i < numberOfPixels; i++) {
+                red += pixels[i].r;
+                green += pixels[i].g;
+                blue += pixels[i].b;
+            }
+            red /= numberOfPixels;
+            green /= numberOfPixels;
+            blue /= numberOfPixels;
+            CGContextRelease(context);
+        }
+        free(pixels);
+    }
+    return [UIColor colorWithRed:red/255.0f green:green/255.0f blue:blue/255.0f alpha:1.0f];
+}
+
+static CGFloat readValue(NSString *key, CGFloat defaultValue) {
+    id r = [[NSUserDefaults standardUserDefaults] objectForKey:key];
+    #if CGFLOAT_IS_DOUBLE
+    return r ? [r doubleValue] : defaultValue;
+    #else
+    return r ? [r floatValue] : defaultValue;
+    #endif
 }
 
 %hook SBIconView
 
-- (void)_updateCloseBoxAnimated:(BOOL)animated
-{
-	%orig;
-	SBCloseBoxView *closeBox = MSHookIvar<SBCloseBoxView *>(self, "_closeBox");
-	if (closeBox != nil && closeBox.hidden == NO) {
-		UIColor *dominantColor = dominantColorFromIcon(self.icon);
-		[MSHookIvar<UIView *>(closeBox, "_whiteTintView") setBackgroundColor:dominantColor];
-		id r = [[NSUserDefaults standardUserDefaults] objectForKey:@"SBCloseBoxTintAlpha"];
-		#if CGFLOAT_IS_DOUBLE
-		CGFloat tintAlpha = r ? [r doubleValue] : 0.65f;
-		#else
-		CGFloat tintAlpha = r ? [r floatValue] : 0.65f;
-		#endif
-		MSHookIvar<UIView *>(closeBox, "_whiteTintView").alpha = tintAlpha;
+- (void)_updateCloseBoxAnimated: (BOOL)animated {
+    %orig;
+    SBCloseBoxView *closeBox = MSHookIvar<SBCloseBoxView *>(self, "_closeBox");
+    if (closeBox && !closeBox.hidden) {
+        UIColor *dominantColor = dominantColorFromIcon(self.icon);
+        MSHookIvar<UIView *>(closeBox, "_whiteTintView").backgroundColor = dominantColor;
+        CGFloat tintAlpha = readValue(@"SBCloseBoxTintAlpha", 0.65);
+        MSHookIvar<UIView *>(closeBox, "_whiteTintView").alpha = tintAlpha;
+        CGFloat borderWidth = readValue(@"SBCloseBoxBorderWidth", 0.0);
+        MSHookIvar<UIView *>(closeBox, "_whiteTintView").layer.borderWidth = borderWidth;
+        if (borderWidth > 0.0)
+            MSHookIvar<UIView *>(closeBox, "_whiteTintView").layer.borderColor = dominantColor.CGColor;
 
-		MSHookIvar<UIView *>(closeBox, "_whiteTintView").layer.borderWidth = 2.0f;
-		MSHookIvar<UIView *>(closeBox, "_whiteTintView").layer.borderColor = dominantColor.CGColor;
-
-		MSHookIvar<UIImageView *>(closeBox, "_xColorBurnView").image = [MSHookIvar<UIImageView *>(closeBox, "_xColorBurnView").image _flatImageWithColor:dominantColor];
-		MSHookIvar<UIView *>(closeBox, "_xColorBurnView").alpha = tintAlpha;
-	}
+        MSHookIvar<UIImageView *>(closeBox, "_xColorBurnView").image = [MSHookIvar<UIImageView *>(closeBox, "_xColorBurnView").image _flatImageWithColor:dominantColor];
+        MSHookIvar<UIView *>(closeBox, "_xColorBurnView").alpha = tintAlpha;
+    }
 }
 
 %end
